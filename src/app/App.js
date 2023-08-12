cd011e54ca9d071802e24b5c60b2fcafa8e959f0
2LeninOFF <118672258+2LeninOFF@users.noreply.github.com>
2023-08-13 03:02:36 +0700

    Add authorization

--- src/app/App.js
+++ src/app/App.js
@@ -5,10 +5,17 @@ import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
+<<<<<<< Updated upstream
+=======
+import { ProfessionProvider } from "./hooks/useProfession";
+import { QualitiesProvider } from "./hooks/useQualities";
+import AuthProvider from "./hooks/useAuth";
+>>>>>>> Stashed changes
 
 function App() {
     return (
         <div>
+<<<<<<< Updated upstream
             <NavBar />
             <Switch>
                 <Route path="/users/:userId?/:edit?" component={Users} />
@@ -16,6 +23,27 @@ function App() {
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
             </Switch>
+=======
+            <AuthProvider>
+                <NavBar />
+
+                <QualitiesProvider>
+                    <ProfessionProvider>
+                        <Switch>
+                            <Route
+                                path="/users/:userId?/:edit?"
+                                component={Users}
+                            />
+                            <Route path="/login/:type?" component={Login} />
+                            <Route path="/" exact component={Main} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </ProfessionProvider>
+                </QualitiesProvider>
+            </AuthProvider>
+
+            <ToastContainer />
+>>>>>>> Stashed changes
         </div>
     );
 }
